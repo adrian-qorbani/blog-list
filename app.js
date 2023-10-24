c7184b8c7f3418e9acd260112ac7c22662848c6f
Adrian <mahdigh@live.com>
2023-10-24 11:31:30 +0330

    fixed a bug where users route couldnt resulted in unknown endpoint.

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 module.exports = app;
 
-app.use("/api/blogs", blogsRouter);
-app.use('/api/users', usersRouter)
+// app.use("/api/blogs", blogsRouter);
