e844357fec9030519caa5fffc4c3541dc88e50bc
Adrian <mahdigh@live.com>
2023-10-24 12:23:05 +0330

    populated blog posts to show each blogs user's name and username.

--- controllers/blog.js
+++ controllers/blog.js
@@ -3,7 +3,7 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 
 blogRouters.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {username: 1, name: 1});
   response.json(blogs);
 });
 
