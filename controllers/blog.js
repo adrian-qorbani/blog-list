59dde99bfa7d12d79a3e464b191bdbb8b1ad7c2f
Adrian <mahdigh@live.com>
2023-10-25 12:36:37 +0330

    exercise 4.20 done. refactored taking the token to middleware.js and registered it in app.js

--- controllers/blog.js
+++ controllers/blog.js
@@ -3,13 +3,13 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
+// const getTokenFrom = (request) => {
+//   const authorization = request.get("authorization");
+//   if (authorization && authorization.startsWith("Bearer ")) {
+//     return authorization.replace("Bearer ", "");
+//   }
+//   return null;
+// };
 
 blogRouters.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -29,7 +29,7 @@ blogRouters.get("/:id", async (request, response, next) => {
 blogRouters.post("/", async (request, response, next) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid." });
   }
