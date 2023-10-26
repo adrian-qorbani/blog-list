9a6137ecc7bc26314448340a0b2fa63f8f03cb22
Adrian <mahdigh@live.com>
2023-10-26 10:03:08 +0330

    added exercise 4.6 - 4.7 which were left behind.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,8 +14,48 @@ const favoriteBlog = (blogs) => {
   return theBlog;
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  } else {
+    let counts = blogs.reduce((count, blog) => {
+      count[blog.author] = (count[blog.author] || 0) + 1;
+      return count;
+    }, {});
+    let max = Math.max(...Object.values(counts));
+    let mostBlogs = Object.keys(counts).filter(
+      (author) => counts[author] === max
+    );
+    return {
+      author: mostBlogs[0],
+      blogs: max,
+    };
+  }
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  } else {
+    let likes = blogs.reduce((like, blog) => {
+      like[blog.author] = (like[blog.author] || 0) + blog.likes;
+      return like;
+    }, {});
+    let max = Math.max(...Object.values(likes));
+    let mostLiked = Object.keys(likes).filter(
+      (author) => likes[author] === max
+    );
+    return {
+      author: mostLiked[0],
+      likes: max,
+    };
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
